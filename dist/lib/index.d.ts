20379bca5513d2e3a5cdaee8a0846dd37871679b
cqh963852 <cqh963852@hotmail.com>
2020-11-29 20:13:46 +0800

    Remove dist

--- dist/lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Dnd } from "./dnd";
-export { default as useDnd } from "./useDnd";
-export * from "./dnd";
-export * from "./useDnd";
--- dist/lib/useDnd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import Dnd from "./dnd";
-export declare const DndContext: import("react").Context<Dnd>;
-declare const useDnd: () => Dnd;
-export default useDnd;
